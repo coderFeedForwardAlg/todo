b560684078eb2bc4620e53999cc1d8e24d1247f0
coderFeedForwardAlg <gaspersscottma@gmail.com>
2025-04-01 03:24:28 +0000

    added more stuff to record

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,22 @@ fn main() -> std::io::Result<()> {
         };
         let mut workData = File::create("workData.txt")?;
         workData.write_all((num + new_pushups).to_string().as_bytes())?;
-         
+        
+        println!("what did you learn");
+        let mut learned_user_input = String::new();
+        io::stdin().read_line(&mut learned_user_input).expect("cant read line");
+        let mut learned_file = File::options().append(true).open("learned.txt")?;
+
+        learned_file.write_all(learned_user_input.to_string().as_bytes())?;
+        //writeln!(&mut learned_file, learned_user_input)?;
+        
+        println!("what do you still need to learn / look back at");
+        let mut need_to_learn_input = String::new();
+        io::stdin().read_line(&mut need_to_learn_input).expect("cant read line");
+        let mut need_learn_file = File::options().append(true).open("need_learn.txt")?;
+        need_learn_file.write_all(need_to_learn_input.to_string().as_bytes())?;
+        
+
         Ok(())
 
 }
